fb73463576bcb2f1854cf2e12f25ce8c8b4a4daf
FavaretoBTZ <vitor@btzmotorsport.com>
2025-10-13 16:13:39 -0300

    Update KPI PERFORMANCE.py

--- KPI PERFORMANCE.py
+++ KPI PERFORMANCE.py
@@ -77,7 +77,7 @@ def _find_col_exact(df: pd.DataFrame, label: str):
 # App
 # =========================
 st.set_page_config(layout="wide")
-st.title("KPI PERFORMANCE - Análise Dinâmica")
+st.title("KPI PERFORMANCE - BTZ|Motorsport")
 
 uploaded_file = st.file_uploader("Escolha a planilha (.xlsx):", type=["xlsx"])
 if not uploaded_file:
